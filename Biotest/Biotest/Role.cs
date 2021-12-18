0a5a28376388ce8c1a423843a5ae53ddbaa56e12
romiswid <swid.romi@gmail.com>
2021-12-18 14:14:04 +0200

    update

---
+++ Biotest/Biotest/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAD1
+{
+    public enum Role
+    {
+        Manager,
+        Secretary,
+        Salesman,
+        SalesmanSecretary
+    }
+}
